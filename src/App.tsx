5178a7e7c923930586e72c99ec94383982d5844d
sfortiz <chinotr@hotmail.com>
2026-07-14 09:32:26 -0600

    Video 118: Poner el marco en el div

--- src/App.tsx
+++ src/App.tsx
@@ -28,8 +28,8 @@ function App() {
           </div>
         </div>
 
-        <div>
-          <h2>Consumo</h2>
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          {/* <h2>Consumo</h2> */}
         </div>
       </main>
     </>
